7e964601533f7c45194f6a7a7128132aa61b55a0
Ayush Agarwal <ayush.agarwal.ggn@gmail.com>
2020-07-02 23:56:40 +0530

    second commit

--- home/models.py
+++ home/models.py
@@ -6,7 +6,7 @@ class table(models.Model):
     sno= models.AutoField(primary_key=True)
     first= models.CharField(max_length=100)
     last= models.CharField(max_length=100)
-    phone= models.IntegerField(max_length=13)
+    phone= models.CharField(max_length=13)
     
     def __str__(self):
         return self.first
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from home.models import table
+from django.contrib import messages
 # Create your views here.
 def home(request):
     tables= table.objects.all()
@@ -8,6 +9,18 @@ def home(request):
 
 def search(request):
     search= request.GET['search']
-    tables=table.objects.filter(first__icontains=search)
+    tablesfirst=table.objects.filter(first__icontains=search)
+    tableslast=table.objects.filter(last__icontains=search)
+    tablesphone=table.objects.filter(phone__icontains=search)
+    tables=tablesfirst.union(tableslast.union(tablesphone))
     params={'tables':tables, 'search':search}
     return render(request,'search.html',params)
+
+def create(request):
+    if request.method=='POST':
+        first = request.POST.get('first')
+        last = request.POST.get('last')
+        phone = request.POST.get('phone')
+        create=table(first=first, last=last, phone=phone)
+        create.save()
+    return redirect('home')
